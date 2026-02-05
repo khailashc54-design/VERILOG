5f9cc0f12619f51f22aba0881ac3eae7211a463a
khailashc54-design <khailashc54@gmail.com>
2026-02-05 11:29:35 +0530

    Add testbench for full adder with half adders

---
+++ adder/data flow /full_adder_2ha_tb.v
@@ -0,0 +1,28 @@
+module fulladded_tb;
+  reg a;
+  reg b;
+  reg cin;
+  wire sum;
+  wire carry;
+  ha_fa dut(
+            .a(a),
+            .b(b),
+            .cin(cin),
+    .sum(sum),
+    .carry(carry)
+  );
+  initial begin
+    $monitor("time=%0t|a=%b b=%b cin=%b| sum=%b carry=%b",
+         $time, a, b, cin, sum, carry);
+    a=0;b=0;cin=0;
+    #10 a=0;b=0;cin=1;
+    #10 a=0;b=1;cin=0;
+    #10 a=0;b=1;cin=1;
+    #10 a=1;b=0;cin=0;
+    #10 a=1;b=0;cin=1;
+    #10 a=1;b=1;cin=0;
+    #10 a=1;b=1;cin=1;
+    $finish;
+  end 
+  endmodule
+    
