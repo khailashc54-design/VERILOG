0bc0eeae20134ef62873ad1927758aa567933c96
khailashc54-design <khailashc54@gmail.com>
2026-02-09 17:43:55 +0530

    Implement testbench for parity checker module
    Added a testbench for the parity module to check even and odd values.

---
+++ parity checker/even.odd b.d.v
@@ -0,0 +1,23 @@
+// Code your design here
+module parity#(parameter n=2)(input [n-1:0]d,output reg odd,even);
+  always@(*)begin
+  even=~(^d);
+   odd=^d;
+  end
+endmodule 
+
+// Code your testbench here
+// or browse Examples
+module parity_tb;
+  parameter n=5;
+  reg [n-1:0]d;
+  wire odd,even;
+  
+  parity#(n)dut(.*);
+  initial begin
+    $monitor("d=%b |even=%b odd=%b",d,even, odd );
+    d=5'b00011;
+    #10d=5'b00111;
+    $finish;
+  end
+endmodule
