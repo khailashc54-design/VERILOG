572362caaf6565d95febcee00f663816e68691ef
khailashc54-design <khailashc54@gmail.com>
2026-02-10 23:29:32 +0530

    Add SR latch and testbench in Verilog

---
+++ combinational ckt/srl_latch_nand_d.v
@@ -0,0 +1,20 @@
+module srlatch(input s,r, output q,qbar);
+  assign q=~(~r&qbar);
+  assign qbar=~(~s&q);
+endmodule
+
+
+module srlatch_tb;
+  reg s,r;
+  wire q,qbar;
+  srlatch dut(.*);
+  initial begin
+  $monitor("t=%0t|s=%b r=%b|q=%b qbar=%b",$time,s,r,q,qbar);
+  s=0;r=0;
+  #10 s=1;r=0;
+  #10 s=0;r=1;
+  #10 s=1;r=1;
+  $finish;
+  end
+  endmodule
+
