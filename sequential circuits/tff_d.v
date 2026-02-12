8030062e65b31981a77e8475ad77bd2f4836eebd
khailashc54-design <khailashc54@gmail.com>
2026-02-12 10:01:51 +0530

    Update T flip-flop module and testbench timing
    Refactor T flip-flop behavior and testbench timing.

--- sequential circuits/tff_d.v
+++ sequential circuits/tff_d.v
@@ -1,29 +1,37 @@
-// Code your design here
 module tff(input t,clk,rst,output reg q,output qbar);
-  always@(posedge clk or rst)begin
-    if(rst)
+  always@(posedge clk or negedge rst)begin
+    if(!rst)
       q<=0;
-    else if(clk & t)
-      q<=~q;
-  end
+    else 
+      begin 
+        case(t)
+          0:q<=q;
+          1:q<=~q;
+          default :q<=1'b0;
+        endcase
+      end 
+      end
       assign qbar=~q;
 endmodule
 
-// Code your testbench here
-// or browse Examples
 module tff_tb;
   reg t,clk,rst;
   wire q, qbar;
   tff dut(.*);
   initial begin 
     $monitor("t=%0t|t=%b clk=%b rest=%b|q=%b qbar=%b",$time,t,clk,rst,q,qbar);
-    t=0;rst=1;
-    #10 t=1;rst=0;
+    t=0;rst=0;
+    #4 t=1;rst=1;
+    #5  t=0;rst=1;
+    #7 t=1;rst=1;
+     #4 t=1;rst=0;
+    #5 t=0;rst=1;
+    #7 t=1;rst=0;
    #20 $finish;
   end
   initial begin
     clk=1'b0;
-    forever#100clk=~clk;
+    forever #5 clk=~clk;
   end
   initial begin
     $dumpfile("tff.vcd");
