ae76267d9a22134248334a1c136805e48e44078e
khailashc54-design <khailashc54@gmail.com>
2026-03-02 21:36:49 +0530

    Create melay _non over_d.v

---
+++ sequential circuits/fsm/melay _non over_d.v
@@ -0,0 +1,42 @@
+// Code your design here
+module melay_fsm_1001( input x,clk,rst,output reg y);
+  reg [1:0]s;
+  parameter s0=2'b00,s1=2'b01,s2=2'b10,s3=2'b11;
+  always @(posedge clk or posedge rst)begin
+    if (rst)
+      s<=s0;
+    else if (s==s0)
+      s<=(x)?s1:s0;
+    else if(s==s1)
+      s<=(x)?s1:s2;
+    else if (s==s2)
+      s<=(x)?s1:s3;
+    else if (s==s3)
+      s<=(x)?s0:s0;
+  end
+  always @(*)begin
+    y=(s==s3 && x==1);
+  end
+endmodule 
+
+// Code your testbench here
+// or browse Examples
+module melay_fsm_1001_tb;
+  reg x,clk,rst;
+  wire y;
+  melay_fsm_1001 dut(.*);
+  initial begin 
+    $monitor ("t=%0t|x=%b clk=%b rst=%b|y=%b",$time,x,clk,rst,y);
+    clk=0;rst=1;
+    x=0;
+    #10 rst=0;
+   #3 x=1;
+    #7 x=0;
+    #10 x=0;
+    #14 x=1;
+    #100$finish;
+  end 
+  initial begin 
+   forever#5 clk = ~clk;
+  end 
+endmodule
