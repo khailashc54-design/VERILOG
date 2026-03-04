c2c797d7bf5799d98a82d108d5a1b6627af58382
khailashc54-design <khailashc54@gmail.com>
2026-03-04 14:39:07 +0530

    Update q15.d.v

--- assignment/dataflow/q15.d.v
+++ assignment/dataflow/q15.d.v
@@ -1,11 +1,12 @@
 module d;
-  reg [3:0]a= 4'b110X;
+  reg [3:0]a= 4'b110x;
 initial begin
-if (a==4'b1100)
-  begin: B1
-end else
-  begin: B2 
-  end
-  $display("a=%b ",a);
+  $monitor("a=%b",a);
+  if ( a !== 4'b1100)
+begin: B1
+end
+else
+begin: B2 end
+ 
 end
 endmodule 
